441b7d49ecd5b947bd8d605bdc7beaa4634bb293
Гевак Владимир Александрович <GevakVA@zrw.oao.rzd>
2018-09-25 10:51:27 +0300

    Init

--- src/store.js
+++ src/store.js
@@ -3,61 +3,81 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let apiLocalStorage = {
+  initialState: function(){
+    return JSON.parse(localStorage.tasks || '[]');
+  },
+  saveState: function(tasks){
+    localStorage.tasks = JSON.stringify(tasks)
+  },
+};
+
+const state = {
+  todos: apiLocalStorage.initialState(),
+};
+
+const getters = {
+  todos: state => {
+    return state.todos
+  },
+  completed: state => {
+    return state.todos.filter((item) => item.isDone)
+  },
+  active: state => {
+    return state.todos.filter((item) => !item.isDone)
+  },
+  id: state => {
+    let id = state.todos.reduce((acc, item) => {
+      if (acc < item.id) return item.id
+      else return acc
+    },-1)
+    return ++id
+  },
+};
+
+const mutations = {
+  addNew(state, payload) {
+    state.todos = [...state.todos, payload];
+    apiLocalStorage.saveState(state.todos)
+  },
+  deleteTask(state, payload){
+    state.todos = state.todos.filter((item) => item.id !== payload)
+    apiLocalStorage.saveState(state.todos)
+  },
+  editTask(state, payload){
+    state.todos = state.todos.map((item) => {
+      if (item.id === payload) item.isEditing = !item.isEditing
+      else item.isEditing = false
+      return item
+    })
+  },
+  saveTask(state, payload){
+    state.todos = state.todos.map((item) => {
+      if (item.id === payload.id) {
+        item.isEditing = false
+        item.task = payload.task
+      }
+      return item
+    });
+    apiLocalStorage.saveState(state.todos);
+  },
+  doneTask(state, payload){
+    state.todos = state.todos.map((item) => {
+      if (item.id === payload.id) {
+        item.isDone = payload.checked
+      }
+      return item
+    });
+    apiLocalStorage.saveState(state.todos);
+  },
+  deleteCompleted(state){
+    state.todos = state.todos.filter((item) => item.isDone == false);
+    apiLocalStorage.saveState(state.todos);
+  }
+};
+
 export default new Vuex.Store({
-  state: {
-    todos: [],
-  },
-  getters:{
-    todos: state => {
-      return state.todos
-    },
-    completed: state => {
-      return state.todos.filter((item) => item.isDone)
-    },
-    active: state => {
-      return state.todos.filter((item) => !item.isDone)
-    },
-    id: state => {
-      let id = state.todos.reduce((acc, item) => {
-        if (acc < item.id) return item.id
-        else return acc
-      },-1)
-      return ++id
-    },
-  },
-  mutations: {
-    addNew(state, payload) {
-      state.todos = [...state.todos, payload]
-    },
-    deleteTask(state, payload){
-      state.todos = state.todos.filter((item) => item.id !== payload)
-    },
-    editTask(state, payload){
-      state.todos = state.todos.map((item) => {
-        if (item.id === payload) item.isEditing = !item.isEditing
-        else item.isEditing = false
-        return item
-      })
-    },
-    saveTask(state, payload){
-      state.todos = state.todos.map((item) => {
-        if (item.id === payload.id) {
-          item.isEditing = false
-          item.task = payload.task
-        }
-        return item
-      })
-    },
-    doneTask(state, payload){
-      state.todos = state.todos.map((item) => {
-        if (item.id === payload.id) {
-          item.isDone = payload.checked
-        }
-        return item
-      })
-    },
-    deleteCompleted(state){
-      state.todos = state.todos.filter((item) => item.isDone == false)
-    }
-  },
+  state,
+  getters,
+  mutations,
 })
